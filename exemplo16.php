55ed33ddb4b147738c498f4e93bfda7ecdab0cfc
Renan Junior de Almeida Silva <renan.junior@docente.senai.br>
2026-04-15 10:58:04 -0300

    Create exemplo16.php

---
+++ exemplo16.php
@@ -0,0 +1,19 @@
+<?php
+
+class Visibilidade {
+    public $varPublic;
+    protected $varProtected;
+    private $varPrivate;
+
+    public function __construct($varPublic, $varProtected, $varPrivate)
+    {
+       $this->varPublic = $varPublic;
+       $this->varProtected = $varProtected;
+       $this->varPrivate = $varPrivate; 
+    }
+}
+
+$teste = new Visibilidade(1,2,3);
+echo "Atributo Public = $teste->varPublic";
+echo "Atributo Protected = $teste->varProtected";
+echo "Atributo Private = $teste->varPrivate";

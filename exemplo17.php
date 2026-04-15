ba4025e3495752bd7e7f74a5880d2ec78c546c33
Renan Junior de Almeida Silva <renan.junior@docente.senai.br>
2026-04-15 11:19:01 -0300

    Create exemplo17.php

---
+++ exemplo17.php
@@ -0,0 +1,38 @@
+<?php
+
+class Visibilidade {
+    public $varPublic;
+    protected $varProtected;
+    private $varPrivate;
+
+    public function __construct($varPublic, $varProtected, $varPrivate)
+    {
+       $this->varPublic = $varPublic;
+       $this->varProtected = $varProtected;
+       $this->varPrivate = $varPrivate; 
+    }
+
+    public function publicFunc(){
+        echo "Método Público<br>";
+    }
+
+    protected function protectedFunc(){
+        echo "Método Protegido<br>";
+    }
+
+    private function privateFunc(){
+        echo "Método Privado<br>";
+    }
+}
+
+$teste = new Visibilidade(1,2,3);
+
+echo "Atributo Public = $teste->varPublic";
+// echo "Atributo Protected = $teste->varProtected";
+// echo "Atributo Private = $teste->varPrivate";
+
+echo "<br>";
+
+$teste->publicFunc();
+$teste->protectedFunc();
+$teste->privateFunc();
